cf5308d2be43197f913775f1f1b8626c694f7232
ParkSungHyun <psh1030.dev@gmail.com>
2022-06-30 20:18:24 +0900

    Feat: 레이아웃 추가
    레이아웃을 맞추기 위해 App.js 하위에 레이아웃을 위한 styled component를
    추가하였습니다.(SectionWrapper ContentWrapper)
    
    "

--- src/App.js
+++ src/App.js
@@ -18,58 +18,73 @@ const TRIPPLE_NUM_OF_PLANS = 470
 
 function App() {
   return (
-    <>
-      <FadeIn
-        duration={FADEIN_ANIMATION_DURATION}
-        delay={TRIPPLE_ICON_FADEIN_DELAY}
-      >
-        <ContentLogo logoSrc={trippleLogoImage}>
-          <div>2019년 2월 기준</div>
-        </ContentLogo>
-      </FadeIn>
-      <FadeIn
-        duration={FADEIN_ANIMATION_DURATION}
-        delay={TRIPPLE_INFO_FADEIN_DELAY}
-      >
-        <IntroduceItemsWrapper>
-          <IntroduceItem
-            itemMaxNumber={TRIPPLE_NUM_OF_TRAVELERS}
-            unit="만 명"
-            description="의 여행자"
-          />
-          <IntroduceItem
-            itemMaxNumber={TRIPPLE_NUM_OF_REVIEWS}
-            unit="만 개"
-            description="의 여행 리뷰"
-          />
-          <IntroduceItem
-            itemMaxNumber={TRIPPLE_NUM_OF_PLANS}
-            unit="만 개"
-            description="의 여행 일정"
-          />
-        </IntroduceItemsWrapper>
-      </FadeIn>
-      <FadeIn
-        duration={FADEIN_ANIMATION_DURATION}
-        delay={TRIPPLE_AWARD_FADEIN_DELAY}
-      >
-        <AwardsWrapper>
-          <AwardItem
-            imageSrc={googleImage}
-            title="2018 구글 플레이스토어"
-            description="올해의 앱 최우수상 수상"
-          />
-          <AwardItem
-            imageSrc={appleImage}
-            title="2018 애플 앱스토어"
-            description="오늘의 여행앱 선정"
-          />
-        </AwardsWrapper>
-      </FadeIn>
-    </>
+    <SectionWrapper>
+      <ContentWrapper>
+        <FadeIn
+          duration={FADEIN_ANIMATION_DURATION}
+          delay={TRIPPLE_ICON_FADEIN_DELAY}
+        >
+          <ContentLogo logoSrc={trippleLogoImage}>
+            <div>2019년 2월 기준</div>
+          </ContentLogo>
+        </FadeIn>
+        <FadeIn
+          duration={FADEIN_ANIMATION_DURATION}
+          delay={TRIPPLE_INFO_FADEIN_DELAY}
+        >
+          <IntroduceItemsWrapper>
+            <IntroduceItem
+              itemMaxNumber={TRIPPLE_NUM_OF_TRAVELERS}
+              unit="만 명"
+              description="의 여행자"
+            />
+            <IntroduceItem
+              itemMaxNumber={TRIPPLE_NUM_OF_REVIEWS}
+              unit="만 개"
+              description="의 여행 리뷰"
+            />
+            <IntroduceItem
+              itemMaxNumber={TRIPPLE_NUM_OF_PLANS}
+              unit="만 개"
+              description="의 여행 일정"
+            />
+          </IntroduceItemsWrapper>
+        </FadeIn>
+        <FadeIn
+          duration={FADEIN_ANIMATION_DURATION}
+          delay={TRIPPLE_AWARD_FADEIN_DELAY}
+        >
+          <AwardsWrapper>
+            <AwardItem
+              imageSrc={googleImage}
+              title="2018 구글 플레이스토어"
+              description="올해의 앱 최우수상 수상"
+            />
+            <AwardItem
+              imageSrc={appleImage}
+              title="2018 애플 앱스토어"
+              description="오늘의 여행앱 선정"
+            />
+          </AwardsWrapper>
+        </FadeIn>
+      </ContentWrapper>
+    </SectionWrapper>
   )
 }
 
+const SectionWrapper = styled.div`
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  background: inherit, center, cover, inherit, none, inherit, inherit, inherit;
+`
+
+const ContentWrapper = styled.div`
+  width: 1040px;
+  height: auto;
+  margin: 0 auto;
+  position: relative;
+`
 const IntroduceItemsWrapper = styled.div`
   margin-left: 623px;
   padding-top: 150px;
