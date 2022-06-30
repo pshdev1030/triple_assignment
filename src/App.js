b8a7326265fd9590959c6481ff5bd567c96e52fe
ParkSungHyun <psh1030.dev@gmail.com>
2022-06-30 20:14:08 +0900

    Feat: tripplelogo 추가
    Tripple 로고를 렌더링하는 컴포넌트를 추가하였습니다.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import styled from '@emotion/styled'
 
 import appleImage from './assets/images/badge-apple4x.png'
 import googleImage from './assets/images/play-store2x.png'
+import trippleLogoImage from './assets/images/triple2x.png'
 import { FadeIn } from './components/FadeIn'
 import { IntroduceItem } from './components/IntroduceItem'
 import { AwardItem } from './components/AwardItem'
 const FADEIN_ANIMATION_DURATION = 700
+const TRIPPLE_ICON_FADEIN_DELAY = 100
 const TRIPPLE_INFO_FADEIN_DELAY = 200
 const TRIPPLE_AWARD_FADEIN_DELAY = 300
 
@@ -16,6 +18,14 @@ const TRIPPLE_NUM_OF_PLANS = 470
 function App() {
   return (
     <>
+      <FadeIn
+        duration={FADEIN_ANIMATION_DURATION}
+        delay={TRIPPLE_ICON_FADEIN_DELAY}
+      >
+        <ContentLogoWrapper logoSrc={trippleLogoImage}>
+          <div>2019년 2월 기준</div>
+        </ContentLogoWrapper>
+      </FadeIn>
       <FadeIn
         duration={FADEIN_ANIMATION_DURATION}
         delay={TRIPPLE_INFO_FADEIN_DELAY}
@@ -58,6 +68,21 @@ function App() {
     </>
   )
 }
+
+const ContentLogoWrapper = styled.div`
+  background-image: url(${(props) => props.logoSrc});
+  background-repeat: no-repeat;
+  background-size: 400px 338px;
+  padding-top: 280px;
+  font-size: 15px;
+  position: absolute;
+  top: 150px;
+  width: 400px;
+  height: 338px;
+  box-sizing: border-box;
+  text-align: center;
+  color: rgba(58, 58, 58, 0.7);
+`
 
 const IntroduceItemsWrapper = styled.div`
   margin-left: 623px;
